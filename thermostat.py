e569a32689cf78adb510056c896bd92fe4ed541d
Bixly <adam@bixly.com>
2014-08-26 11:55:03 -0700

    initial exapmle

---
+++ thermostat.py
@@ -0,0 +1,15 @@
+class thermostat(NebriOS):
+    # this KVP is updated from another script
+    # that checks the thermostat
+    listens_to == ['shared.temperature'] 
+
+    # determine if the script should run
+    def check(self):
+        return shared.temperature > 79 
+
+    # if the check is true, action() runs
+    def action(self):
+        send_email("manager@example.com", "Temperature Adjusted")
+
+        # install and call any Python package 
+        thermostat.set(75)
