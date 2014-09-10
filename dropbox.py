0f1592d7446e811281d77310edd73ca98f9e969b
Jonathan Kosgei <jonathankosgei1000@gmail.com>
2014-09-10 12:23:15 +0000

    Update dropbox.py
    Added some comments.

--- dropbox.py
+++ dropbox.py
@@ -3,30 +3,40 @@ import time
 import re
 from datetime import datetime
 
+#App key and secret to be acquired from the Dropbox developer site.
+app_key = 'APP_KEY'
+app_secret = 'APP_SECRET'
 
-app_key = '07gz8s75oxfvmbg'
-app_secret = 'tf94vvgp96unnrf'
-
+#Starting the Authorization flow
 flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
 
+#Instead of a direct redirect here we simply print the url
 authorize_url = flow.start()
 print '1. Go to: ' + authorize_url
 print '2. Click "Allow" (you might have to log in first)'
 print '3. Copy the authorization code.'
+
+#What this does is prompt the user to input the Authorization code
 code = raw_input("Enter the authorization code here: ").strip()
 
-
+#We use the Auth Code to request an access_token which we can store and associate with a particular user.
 access_token, user_id = flow.finish(code)
 
+#We define a function to interact with the date, time property returned by the dropbox API when we run a search later on
 def __datetime(date_str):
     return datetime.strptime(date_str, "%a, %d %b %Y %H:%M:%S +0000")
 
+#We begin the integration with NebriOS
 class dropbox_monitor(NebriOS):
+	#Setting the script to run hourly
     schedule = "0 * * * *"
 
     def check(self):
+    	#Create a client object
         client = dropbox.client.DropboxClient(access_token)
-	search = client.search("/", "start", file_limit=1000, include_deleted=False)
+	#Run a search for the file report.otd in our root Dropbox folder
+	search = client.search("/", "report.otd", file_limit=1000, include_deleted=False)
+	#A check for results
 	if(search[0]):
 		for i in search:
 			current_time = time.strftime("%a, %d %b %Y %H:%M:%S +0000")
