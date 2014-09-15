86ebbe4495af1b22c683ffef81fe1cb9da968758
Jonathan Kosgei <jonathankosgei1000@gmail.com>
2014-09-15 11:52:15 +0000

    Update dropbox.py

--- dropbox.py
+++ dropbox.py
@@ -1,55 +1,44 @@
 import dropbox
-import time
-import re
-from datetime import datetime
-
-#App key and secret to be acquired from the Dropbox developer site.
-app_key = 'APP_KEY'
-app_secret = 'APP_SECRET'
-
-#Starting the Authorization flow
-flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
-
-#Instead of a direct redirect here we simply print the url
-authorize_url = flow.start()
-print '1. Go to: ' + authorize_url
-print '2. Click "Allow" (you might have to log in first)'
-print '3. Copy the authorization code.'
-
-#What this does is prompt the user to input the Authorization code
-code = raw_input("Enter the authorization code here: ").strip()
-
-#We use the Auth Code to request an access_token which we can store and associate with a particular user.
-access_token, user_id = flow.finish(code)
-
-#We define a function to interact with the date, time property returned by the dropbox API when we run a search later on
-def __datetime(date_str):
-    return datetime.strptime(date_str, "%a, %d %b %Y %H:%M:%S +0000")
-
-#We begin the integration with NebriOS
+ 
 class dropbox_monitor(NebriOS):
-	#Setting the script to run hourly
+  #Hourly cron for now, still poking at drips
     schedule = "0 * * * *"
-
+  #The script is started by setting "dropbox_monitor_go to true as you suggested which starts the flow, the "code" is to restart the script when the confirmation code is enterd via an email form""
+    listens_to = ["dropbox_monitor_go", "code"]
+  #class attributes
+    app_key = 'APP_KEY'
+    app_secret = 'APP_SECRET'
+  #Start the flow so as to populate the authorize_url variable to be emailed later if code isn't set.
+    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
+    authorize_url = flow.start()
+    
+    def __datetime(self, date_str):
+    return datetime.strptime(date_str, "%a, %d %b %Y %H:%M:%S +0000")
+    
     def check(self):
-    	#Create a client object
-        client = dropbox.client.DropboxClient(access_token)
-	#Run a search for the file report.otd in our root Dropbox folder
-	search = client.search("/", "report.otd", file_limit=1000, include_deleted=False)
-	#A check for results
-	if(search[0]):
-		for i in search:
-			current_time = time.strftime("%a, %d %b %Y %H:%M:%S +0000")
-			modified_time = search[0]['modified']
-			start = __datetime(modified_time)
-			end = __datetime(current_time)
-			delta = end - start
-			days = int(delta.days)
-			if(days <= 7):
-				return True
-			else:
-				return False
+      #check if the code (the confirmation code from dropbox) is set
+      if self.code is not None:
+		    access_token, user_id = flow.finish(self.code)
+		    client = dropbox.client.DropboxClient(access_token)
+		    search = client.search("/", "report.otd", file_limit=1000, include_deleted=False)
+	    	if(search[0]):
+			    for i in search:
+				    current_time = time.strftime("%a, %d %b %Y %H:%M:%S +0000")
+				    modified_time = search[0]['modified']
+				    start = self.__datetime(modified_time)
+				    end = self.__datetime(current_time)
+			    	delta = end - start
+				    days = int(delta.days)
+				    if(days <= 7):
+				    	return True
+				    else:
+				    	return False
+		    else:
+		    	return 'File does not exist.'
 	else:
-		return 'File does not exist.'
+	  #If it isn't set an email is sent with an authorize_url to be clicked to reveal the code, the email also avails a form to fill in the code, once this KVP is set/updated the script is restarted
+		send_email(self.last_actor,
+            """Hello, Please click the link and enter the confirmation code in the form {{authorize_url}} code := - Please enter the confirmation code
+-Nebri OS""", subject="Nebri OS Dropbox")
     def action(self):
         self.new_file = True
