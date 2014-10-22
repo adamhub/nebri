1fdd3efca77ee8d67c9ff93c59c4bb0e2297c420
craktok <king.thomas.c@gmail.com>
2014-10-22 15:10:11 -0700

    updated the scripts

--- smartthings/scripts/st_send_command.py
+++ smartthings/scripts/st_send_command.py
@@ -5,7 +5,7 @@ class st_send_command(NebriOS):
     listens_to = ['st_command']
 
     def check(self):
-        if self.st_command and shared.st_access_token and self.st_device:
+        if self.st_command and self.st_device_label and shared.st_access_token:
             return True
         elif not shared.st_access_token:
             send_email(self.last_actor,
@@ -18,9 +18,38 @@ class st_send_command(NebriOS):
         else:
             return False
 
-    def action(self):
-        self.st_status = "Ran Command -- %s -- with arguments -- %s -- at: %s" % (self.st_command, self.st_command_arguments, datetime.now())
 
+    def action(self):
+
+        self.st_status = "Ran Command -- %s -- with arguments -- %s -- on device label -- %s -- at: %s" % (self.st_command, self.st_command_arguments, self.st_device_label, datetime.now())
+
+        st = self.st_api_connection()
+
+        if not shared.st_devices:
+            self.st_get_devices(st)
+
+        for device in shared.st_devices:
+            if device['label'] == self.st_device_label:
+                self.st_device = {"id": device['id'], "type": device['type'], "label": device['label']}
+                break
+
+
+        ds = st.request_devices(self.st_device['type'])
+
+        ds = filter(lambda d: self.st_device['id'] in [ d.get("id"), d.get("label"), ], ds)
+
+        requestd = {"command": self.st_command}
+
+        if self.st_command_arguments:
+            requestd["arguments"] = self.st_command_arguments
+
+        for d in ds:
+            self.st_response = st.device_request(d, requestd)
+        # This is a loop cause I'm guessing there's a way to interact with multiple Devices,
+        # but for now, this should only ever have 1 device in the "ds" list
+
+
+    def st_api_connection(self):
         st = smartthings.SmartThings(verbose=True)
         st.std = {
             'access_token': shared.st_access_token,
@@ -32,28 +61,20 @@ class st_send_command(NebriOS):
             'expires_in': 1576799999,
             'token_type': 'bearer',
         }
-
         st.request_endpoints()
-        # I'm just translating from the example script.
+        return st
 
-        ds = st.request_devices(self.st_device['type'])
-        # I have manually entered the Device Type of "switch" so that we can talk with the Ge Link light bulb in Nick's Office
 
-        # ds = st.request_devices(self.device_type)
-        # We'll need to see what "devices" actually holds
+    def st_get_devices(self, st):
+        device_type = ['switch', 'motion', 'temperature', 'contact', 'acceleration', 'presence', 'battery', 'threeAxis']
 
-        ds = filter(lambda d: self.st_device['id'] in [ d.get("id"), d.get("label"), ], ds)
-        # I have manually entered the Device ID of the GE Link light bulb in Nick's Office.
+        shared.st_devices = []
 
-        # ds = filter(lambda d: self.device_id in [ d.get("id"), d.get("label"), ], ds)
-        # This will work once we actually have the KVP for device_id setup
-
-        requestd = {"command": self.st_command}
-
-        if self.st_command_arguments:
-            requestd["arguments"] = self.st_command_arguments
-
-        for d in ds:
-            self.st_response = st.device_request(d, requestd)
-        # This is a loop cause I'm guessing there's a way to interact with multiple Devices,
-        # but for now, this should only ever have 1 device in the "ds" list
+        for type in device_type:
+            ds = st.request_devices(type)
+            for d in ds:
+                shared.st_devices.append({
+                    'type': d['type'],
+                    'id': d['id'],
+                    'label': d['label'] if d['label'] else d['name'],
+                });
