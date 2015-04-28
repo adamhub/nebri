697377a34ff720aa6653e4e76de4d1e26cdf40a0
adamhub <adam@bixly.com>
2015-04-28 13:26:12 -0700

    Update google_drive_auth.py

--- google-drive/google_drive_auth.py
+++ google-drive/google_drive_auth.py
@@ -1,25 +1,32 @@
 import httplib2
 
 from apiclient.discovery import build
-from apiclient.http import MediaFileUpload
 from oauth2client.client import OAuth2WebServerFlow
 
-OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
-REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
+# scope now includes google docs spreadsheet as well as the drive
+OAUTH_SCOPE = ['https://www.googleapis.com/auth/drive',
+               'https://spreadsheets.google.com/feeds',
+               'https://docs.google.com/feeds']
+REDIRECT_URI = 'https://www.example.com/oauth2callback'
 
-class google_drive_auth(NebriOS):
-    listens_to = ['shared.drive_key', 'shared.drive_secret', 'shared.drive_code', 'drive_test']
 
+class google_auth(NebriOS):
+    listens_to = ['shared.client_key', 'shared.client_secret',
+                  'shared.client_code', 'googleauth_test']
+    
     def check(self):
-        return shared.drive_key and shared.drive_secret
+        return shared.client_key and shared.client_secret
 
     def action(self):
-        flow = OAuth2WebServerFlow(shared.drive_key, shared.drive_secret, OAUTH_SCOPE, REDIRECT_URI)
-        if shared.drive_code:
-            shared.drive_credentials = flow.step2_exchange(shared.drive_code).to_json()
+        flow = OAuth2WebServerFlow(shared.client_key, shared.client_secret,
+                                   OAUTH_SCOPE, REDIRECT_URI)
+        authorize_url = flow.step1_get_authorize_url()
+
+        if shared.client_code:
+            shared.client_credentials = flow.step2_exchange(shared.client_code).to_json()
         else:
             self.authorize_url = flow.step1_get_authorize_url()
-            send_email("tomk@bixly.com",
+            send_email("keving@bixly.com",
                 "Hello, Please click the link and enter the confirmation code in the form {{authorize_url}} shared_drive_code := - Please enter the confirmation code\n-Nebri OS",
-                subject="Nebri OS Google Drive"
+                subject="Nebri OS Google Auth"
             )
