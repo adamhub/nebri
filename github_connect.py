3c5f24da7795e58b4625ab4af97ea9b127068fb6
nebri-scripts <adam@nebrios.com>
2015-08-11 14:05:16 -0700

    simple github connector, shows commits of a user

---
+++ github_connect.py
@@ -0,0 +1,27 @@
+import datetime
+from github import Github
+
+# When triggering a wake-up, repo_name needs to be sent.
+class github_connect(NebriOS):
+    listens_to = ['github_connect']
+
+    def check(self):
+        return self.github_connect == True
+
+    def action(self):
+        self.github_connect = "RAN"
+        g = Github(shared.GITHUB_USERNAME, shared.GITHUB_PASSWORD)
+        try:
+            repo = g.get_repo(self.repo_name)
+        except Exception as e:
+            print e
+            self.error = str(e)
+        else:
+            since = datetime.datetime.now() - datetime.timedelta(days=1)
+            commits = repo.get_commits(since=since)
+            # We have to iterate to get number of commits because
+            # return value of above call is a pagination
+            self.counter = 0
+            for commit in commits:
+                self.counter += 1
+            print "Number of commits since %s: %s" % (since, self.counter)
