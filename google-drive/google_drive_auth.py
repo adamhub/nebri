d21d58e54d2405876245da49c2eaccb2b6facae4
nebri-scripts <adam@nebrios.com>
2015-08-11 14:17:06 -0700

    kvp docs

--- google-drive/google_drive_auth.py
+++ google-drive/google_drive_auth.py
@@ -9,6 +9,15 @@ OAUTH_SCOPE = ['https://www.googleapis.com/auth/drive',
                'https://docs.google.com/feeds']
 REDIRECT_URI = 'https://www.example.com/oauth2callback'
 
+"""
+shared.client_key - string. app's client id from google developers
+shared.client_secret - string. app's client secret from google developers
+shared.client_code - string. adde by user with code generated from auth url sent in email
+shared.client_credentials - string. auto-filled by script
+shared.update_gsheet - dict. contains spreadsheet filename and list of data to insert
+ex: {"data": [["583.33", "Jone", "false"], ["58.4", "Kit", "false"], ["1083.27", "Nothing Jordan", "true"]], "filename": "test"}
+"""
+
 
 class google_auth(NebriOS):
     listens_to = ['shared.client_key', 'shared.client_secret',
